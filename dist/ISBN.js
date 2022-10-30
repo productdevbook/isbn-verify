c5369196d21c7d0c962e31f84a6d595e9fba4dff
SaekiTominaga <kcsu3470+github@gmail.com>
2022-10-30 22:04:00 +0900

    fix #8 ES2020 → ES2022

--- dist/ISBN.js
+++ dist/ISBN.js
@@ -1,50 +1,36 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
-};
-var _isbnNoHyphens, _isbn13, _isbn10;
 /**
  * ISBN Verify
  */
 export default class {
+    #isbnNoHyphens; // ハイフンなしの ISBN
+    #isbn13 = false; // 現行規格（13桁）の ISBN か
+    #isbn10 = false; // 旧規格（10桁）の ISBN か
     /**
      * @param {string} isbn - ISBN value to check
      * @param {boolean} strict - Strict mode. If `true`, syntax without hyphens is an error. If not specified, it defaults to `false`
      */
     constructor(isbn, strict = false) {
-        _isbnNoHyphens.set(this, void 0); // ハイフンなしの ISBN
-        _isbn13.set(this, false); // 現行規格（13桁）の ISBN か
-        _isbn10.set(this, false); // 旧規格（10桁）の ISBN か
         const isbnNoHyphens = isbn.replace(/-/g, '');
-        __classPrivateFieldSet(this, _isbnNoHyphens, isbnNoHyphens);
+        this.#isbnNoHyphens = isbnNoHyphens;
         if (strict) {
             const length = isbn.length;
             if (length === 17 && /^(978|979)-\d{1,5}-\d{1,7}-\d{1,7}-\d$/.test(isbn)) {
-                __classPrivateFieldSet(this, _isbn13, true);
+                this.#isbn13 = true;
             }
             else if (length === 13 && /^\d{1,5}-\d{1,7}-\d{1,7}-[\dX]$/.test(isbn)) {
-                __classPrivateFieldSet(this, _isbn10, true);
+                this.#isbn10 = true;
             }
         }
         else {
             if (!isbn.includes('--')) {
                 if (/^(978|979)\d{10}$/.test(isbnNoHyphens)) {
                     if (/^\d[\d-]{11,15}\d$/.test(isbn)) {
-                        __classPrivateFieldSet(this, _isbn13, true);
+                        this.#isbn13 = true;
                     }
                 }
                 else if (/^\d{9}[\dX]$/.test(isbnNoHyphens)) {
                     if (/^\d[\d-]{8,11}[\dX]$/.test(isbn)) {
-                        __classPrivateFieldSet(this, _isbn10, true);
+                        this.#isbn10 = true;
                     }
                 }
             }
@@ -67,9 +53,9 @@ export default class {
      */
     isIsbn13(options) {
         if (options !== undefined && options.check_digit) {
-            return __classPrivateFieldGet(this, _isbn13) && this.verifyCheckDigit();
+            return this.#isbn13 && this.verifyCheckDigit();
         }
-        return __classPrivateFieldGet(this, _isbn13);
+        return this.#isbn13;
     }
     /**
      * Whether it is a 10-digit ISBN
@@ -80,9 +66,9 @@ export default class {
      */
     isIsbn10(options) {
         if (options !== undefined && options.check_digit) {
-            return __classPrivateFieldGet(this, _isbn10) && this.verifyCheckDigit();
+            return this.#isbn10 && this.verifyCheckDigit();
         }
-        return __classPrivateFieldGet(this, _isbn10);
+        return this.#isbn10;
     }
     /**
      * Verify format (do not verify check digit)
@@ -90,7 +76,7 @@ export default class {
      * @returns {boolean} `true` if the format is correct
      */
     verifyFormat() {
-        return __classPrivateFieldGet(this, _isbn13) || __classPrivateFieldGet(this, _isbn10);
+        return this.#isbn13 || this.#isbn10;
     }
     /**
      * Verify format including check digit (not necessarily applicable publication)
@@ -98,12 +84,12 @@ export default class {
      * @returns {boolean} `true` if both format and check digit are correct
      */
     verifyCheckDigit() {
-        if (__classPrivateFieldGet(this, _isbn13)) {
-            const isbnNoHyphens = __classPrivateFieldGet(this, _isbnNoHyphens);
+        if (this.#isbn13) {
+            const isbnNoHyphens = this.#isbnNoHyphens;
             return isbnNoHyphens.substring(12) === this._getCheckDigit13(isbnNoHyphens);
         }
-        else if (__classPrivateFieldGet(this, _isbn10)) {
-            const isbnNoHyphens = __classPrivateFieldGet(this, _isbnNoHyphens);
+        else if (this.#isbn10) {
+            const isbnNoHyphens = this.#isbnNoHyphens;
             return isbnNoHyphens.substring(9) === this._getCheckDigit10(isbnNoHyphens);
         }
         return false;
@@ -164,5 +150,4 @@ export default class {
         return checkDigit;
     }
 }
-_isbnNoHyphens = new WeakMap(), _isbn13 = new WeakMap(), _isbn10 = new WeakMap();
 //# sourceMappingURL=ISBN.js.map
